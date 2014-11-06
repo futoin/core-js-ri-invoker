3f2178b6ddecc6c8dcabf93a00f3b9fecf4382cb
Andrey Galkin <andrey@futoin.eu>
2014-11-06 17:59:40 +0200

    Refactored to avoid circle dependencies between source files

--- test/unittest.js
+++ test/unittest.js
@@ -25,18 +25,28 @@ describe( 'SimpleCCM', function()
         ccm = new invoker.SimpleCCM();
     });
     
-    it('should register interface', function( done ){
-        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
-        as.add(function(as){ done(); as.success(); });
-        as.execute();
-    });
+    it('should register interface',
+        function( done ){
+            try
+            {
+                ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
+            }
+            catch ( e )
+            {
+                console.log( as.state.error_info );
+                throw e;
+            }
+            as.add(function(as){ done(); as.success(); });
+            as.execute();
+        }
+    );
     
     it('should unregister interface', function( done ){
         ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
         ccm.register( as , 'otherface', 'iface.b:1.2', 'http://localhost:12345' );
         
         as.add(
-            function(as){
+            function(as){try{
                 ccm.assertIface( 'myiface', 'iface.a:1.1' );
                 ccm.assertIface( 'otherface', 'iface.b:1.2' );
                 
@@ -49,14 +59,81 @@ describe( 'SimpleCCM', function()
                 
                 as.success();
                 done();
-            },
+            } catch ( e ){
+                console.dir( e.stack );
+                console.log( as.state.error_info );
+                throw e;
+            }},
             function( as, err )
             {
                 console.log( err + ": " + as.state.error_info );
+            }
+        );
+        as.execute();
+    });
+    
+    it('should create interface alias', function( done ){
+        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
+        ccm.register( as , 'otherface', 'iface.b:1.2', 'http://localhost:12345' );
+        
+        as.add(
+            function(as){try{
+                ccm.assertIface( 'myiface', 'iface.a:1.0' );
+                ccm.assertIface( 'otherface', 'iface.b:1.2' );
+                
+                ccm.alias( 'myiface', 'newname' );
+
+                ccm.assertIface( 'newname', 'iface.a:1.0' );
+
+                assert.throws( function(){
+                    ccm.assertIface( 'myiface', 'iface.a:1.3' );
+                }, 'InvokerError' );
+                
+                assert.throws( function(){
+                    ccm.assertIface( 'myiface', 'iface.a:2.0' );
+                }, 'InvokerError' );
+
+                assert.equal( ccm.iface('myiface'), ccm.iface('newname') );
+                
+                as.success();
                 done();
+            } catch ( e ){
+                console.dir( e.stack );
+                console.log( as.state.error_info );
+                throw e;
+            }},
+            function( as, err )
+            {
+                console.log( err + ": " + as.state.error_info );
             }
         );
         as.execute();
     });
 
 } );
+
+describe( 'AdvancedCCM', function()
+{
+    beforeEach(function(){
+        as = async_steps();
+        var opts = {};
+        opts[ invoker.AdvancedCCM.OPT_SPEC_DIRS ] = __dirname + '/specs';
+        ccm = new invoker.AdvancedCCM( opts );
+    });
+    
+    it('should register interface',
+        function( done ){
+            try
+            {
+                ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:12345' );
+            }
+            catch ( e )
+            {
+                console.log( as.state.error_info );
+                throw e;
+            }
+            as.add(function(as){ done(); as.success(); });
+            as.execute();
+        }
+    );
+});
