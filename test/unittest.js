94ea463a0cf0af59ff38813411fced548e70c49b
Andrey Galkin <andrey@futoin.eu>
2014-12-01 03:45:14 +0200

    A set of bugfixes during initial testing

--- test/unittest.js
+++ test/unittest.js
@@ -5,6 +5,9 @@ var invoker;
 var as;
 var ccm;
 var _ = require( 'lodash' );
+var http = require('http');
+var httpsrv;
+var url = require( 'url' );
 
 describe( 'Invoker Basic', function()
 {
@@ -30,7 +33,7 @@ describe( 'SimpleCCM', function()
         function( done ){
             try
             {
-                ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
+                ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:23456' );
             }
             catch ( e )
             {
@@ -43,8 +46,8 @@ describe( 'SimpleCCM', function()
     );
     
     it('should unregister interface', function( done ){
-        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
-        ccm.register( as , 'otherface', 'iface.b:1.2', 'http://localhost:12345' );
+        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:23456' );
+        ccm.register( as , 'otherface', 'iface.b:1.2', 'http://localhost:23456' );
         
         as.add(
             function(as){try{
@@ -74,8 +77,8 @@ describe( 'SimpleCCM', function()
     });
     
     it('should create interface alias', function( done ){
-        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
-        ccm.register( as , 'otherface', 'iface.b:1.2', 'http://localhost:12345' );
+        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:23456' );
+        ccm.register( as , 'otherface', 'iface.b:1.2', 'http://localhost:23456' );
         
         as.add(
             function(as){try{
@@ -115,9 +118,9 @@ describe( 'SimpleCCM', function()
         as.state.fire_reg = false;
         as.add(
             function( as ){
-                ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
+                ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:23456' );
                 as.state.fire_reg = true;
-                ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
+                ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:23456' );
                 as.successStep();
             },
             function( as, err ){try{
@@ -137,18 +140,18 @@ describe( 'SimpleCCM', function()
     it('should fail on invalid ifacever at registration/assert', function(){
         
         assert.throws(function(){
-            ccm.register( as , 'myiface', 'iface.a:1.', 'http://localhost:12345' );
+            ccm.register( as , 'myiface', 'iface.a:1.', 'http://localhost:23456' );
         }, 'InvokerError' );
         
         assert.throws(function(){
-            ccm.register( as , 'myiface', 'iface.a.1.0', 'http://localhost:12345' );
+            ccm.register( as , 'myiface', 'iface.a.1.0', 'http://localhost:23456' );
         }, 'InvokerError' );
         
         assert.throws(function(){
-            ccm.register( as , 'myiface', 'iface$%$%.a.1.0', 'http://localhost:12345' );
+            ccm.register( as , 'myiface', 'iface$%$%.a.1.0', 'http://localhost:23456' );
         }, 'InvokerError' );
         
-        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:12345' );
+        ccm.register( as , 'myiface', 'iface.a:1.1', 'http://localhost:23456' );
         
         assert.throws(function(){
             ccm.assertIface( 'myiface', 'iface.a' );
@@ -187,19 +190,19 @@ describe( 'SimpleCCM', function()
     });
     
     it('should marked endpoints as secure', function(){
-        ccm.register( as, 'myhttp', 'a:1.0', 'http://localhost:12345' );
+        ccm.register( as, 'myhttp', 'a:1.0', 'http://localhost:23456' );
         ccm._iface_info.myhttp.secure_channel.should.be.false;
-        ccm.register( as, 'myhttps', 'a:1.0', 'https://localhost:12345' );
+        ccm.register( as, 'myhttps', 'a:1.0', 'https://localhost:23456' );
         ccm._iface_info.myhttps.secure_channel.should.be.true;
-        ccm.register( as, 'myws', 'a:1.0', 'ws://localhost:12345' );
+        ccm.register( as, 'myws', 'a:1.0', 'ws://localhost:23456' );
         ccm._iface_info.myws.secure_channel.should.be.false;
-        ccm.register( as, 'mywss', 'a:1.0', 'wss://localhost:12345' );
+        ccm.register( as, 'mywss', 'a:1.0', 'wss://localhost:23456' );
         ccm._iface_info.mywss.secure_channel.should.be.true;
-        ccm.register( as, 'mysechttp', 'a:1.0', 'secure+http://localhost:12345' );
+        ccm.register( as, 'mysechttp', 'a:1.0', 'secure+http://localhost:23456' );
         ccm._iface_info.mysechttp.secure_channel.should.be.true;
-        ccm.register( as, 'mysecws', 'a:1.0', 'secure+ws://localhost:12345' );
+        ccm.register( as, 'mysecws', 'a:1.0', 'secure+ws://localhost:23456' );
         ccm._iface_info.mysecws.secure_channel.should.be.true;
-        ccm.register( as, 'myunix', 'a:1.0', 'unix://localhost:12345' );
+        ccm.register( as, 'myunix', 'a:1.0', 'unix://localhost:23456' );
         ccm._iface_info.myunix.secure_channel.should.be.true;
     });
     
@@ -216,8 +219,8 @@ describe( 'SimpleCCM', function()
     });
     
     it('should properly properly manage aliases', function(){
-        ccm.register( as , 'myifacea', 'iface.a:1.0', 'http://localhost:12345' );
-        ccm.register( as , 'myifaceb', 'iface.b:1.1', 'http://localhost:12345' );
+        ccm.register( as , 'myifacea', 'iface.a:1.0', 'http://localhost:23456' );
+        ccm.register( as , 'myifaceb', 'iface.b:1.1', 'http://localhost:23456' );
         
         ccm.alias( 'myifacea', 'aiface1' );
         ccm.alias( 'myifacea', 'aiface2' );
@@ -274,7 +277,7 @@ describe( 'AdvancedCCM', function()
         function( done ){
             try
             {
-                ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:12345' );
+                ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:23456' );
             }
             catch ( e )
             {
@@ -292,12 +295,12 @@ describe( 'AdvancedCCM', function()
             as.add(
                 function( as )
                 {
-                    ccm.initFromCache( as, 'http://localhost:12345' );
+                    ccm.initFromCache( as, 'http://localhost:23456' );
                     as.successStep();
                 },
                 function( as, err )
                 {
-                    ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:12345' );
+                    ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:23456' );
                     ccm.cacheInit( as );
                     as.successStep();
                     done();
@@ -309,6 +312,224 @@ describe( 'AdvancedCCM', function()
     );
 });
 
+//============================================================================
+function processTestServerRequest( request, data )
+{
+    var freq;
+
+    if ( request.url !== '/ftn' )
+    {
+        var parsed_url = url.parse( request.url, true );
+        var path = parsed_url.path.split('/');
+        
+        freq = {
+            f : path[2] + ':' + path[3] + ':' + path[4],
+            p : parsed_url.query
+        };
+        
+        if ( path[5] )
+        {
+            freq.sec = path[5];
+        }
+    }
+    else
+    {
+        freq = JSON.parse( data );
+    }
+    
+    var func = freq.f.split(':');
+
+    if ( func.length !== 3 )
+    {
+        return { e : 'InvalidReuquest' };
+    }
+    else if ( func[0] !== 'fileface.a' )
+    {
+        return { e : 'UnknownInterface' };
+    }
+    else if ( func[1] !== '1.1' )
+    {
+        return { e : 'NotSupportedVersion' };
+    }
+    
+    switch ( func[2] )
+    {
+        case 'testFunc' :
+            freq.p.a.should.equal( '1' );
+            freq.p.n.should.equal( 2 );
+            freq.p.o.m.should.equal( 3 );
+            return { res : 'MY_RESULT' };
+        
+        case 'noResult' :
+            freq.p.a.should.equal( '123' );
+            return {};
+            
+        case "call" :
+            freq.p.should.be.empty;
+            return {};
+            
+        case "rawUploadFunc" :
+            freq.p.should.be.empty;
+            data.should.equal( "MY_UPLOAD" );
+            return { ok : "OK" };
+
+        case "rawUploadFuncParams" :
+            freq.p.a.should.equal( '123' );
+            data.should.equal( "MY_UPLOAD" );
+            return { ok : "OK" };
+            
+        case "rawDownload" :
+            freq.p.should.be.empty;
+            return "MY_DOWNLOAD";
+            
+        case "triggerError":
+            return { e : 'MY_ERROR' };
+    }
+    
+    return { e : 'InvalidFunction' };
+}
+
+function createTestHttpServer( cb )
+{
+    if ( httpsrv )
+    {
+        cb();
+        return;
+    }
+
+    httpsrv = http.createServer(function (request, response) {
+        var freq = [];
+        
+        request.on( "data",function( chunk ) {
+            freq.push( chunk );
+        } );
+        request.on( "end",function(){
+            freq = Buffer.concat( freq ).toString( 'utf8' );
+            
+            var frsp = processTestServerRequest( request, freq );
+            var content_type;
+            
+            if ( typeof frsp !== 'string' )
+            {
+                if ( !( 'e' in frsp ) )
+                {
+                    frsp = { r : frsp };
+                }
+                
+                frsp = JSON.stringify( frsp );
+                content_type = 'application/futoin+json';
+            }
+            else
+            {
+                content_type = 'application/octet-stream';
+            }
+            
+            response.writeHead( 200, {
+                'Content-Type' : content_type,
+                'Content-Length' : Buffer.byteLength( frsp, 'utf8' ),
+            } );
+            response.write( frsp, 'utf8' );
+            response.end();
+        });
+    });
+    httpsrv.listen( 23456, '127.0.0.1', 10, cb );
+}
+
+var call_remotes_model_as = async_steps();
+call_remotes_model_as.add(
+    function(as){
+        var iface = ccm.iface( 'myiface' );
+
+        as.add(function(as){
+            iface.call(
+                as,
+                'testFunc',
+                {
+                    a : "1",
+                    n : 2,
+                    o : { m : 3 }
+                }
+            );
+        }).add(function(as, res){
+            res.res.should.equal('MY_RESULT');
+            
+            iface.call(
+                as,
+                "noResult",
+                {
+                    a : "123"
+                }
+            );
+        }).add(function(as, res){
+            res.should.be.empty;
+
+            iface.call(
+                as,
+                "call"
+            );
+        }).add(function(as, res){ try{
+            res.should.be.empty;
+
+            iface.call(
+                as,
+                "rawUploadFunc",
+                {},
+                "MY_UPLOAD"
+            );
+        } catch ( e ) {
+            console.dir( e.stack );
+            console.log( as.state.error_info );
+            throw e;
+        }} ).add(function(as, res){ try{
+            res.ok.should.equal("OK");
+
+            iface.call(
+                as,
+                "rawUploadFuncParams",
+                {
+                    a : "123"
+                },
+                "MY_UPLOAD"
+            );
+        } catch ( e ) {
+            console.dir( e.stack );
+            console.log( as.state.error_info );
+            throw e;
+        }}).add(function(as, res){
+            res.ok.should.equal("OK");
+
+            iface.call(
+                as,
+                "rawDownload"
+            );
+        }).add(
+            function(as, res){
+                res.should.equal("MY_DOWNLOAD");
+
+                iface.call(
+                    as,
+                    "triggerError"
+                );
+            },
+            function( as, err )
+            {
+                err.should.equal("MY_ERROR");
+                as.success( "YES" );
+            }
+        ).add(function(as, res){
+            res.should.equal("YES");
+            as.success();
+        });
+    },
+    function( as, err )
+    {
+        as.state.done( new Error( err + ": " + as.state.error_info ) );
+    }
+).add( function( as ){
+    as.state.done();
+});;
+
+//============================================================================
 describe( 'NativeIface', function()
 {
     describe('#ifaceInfo() - SimpleCCM', function(){
@@ -318,8 +539,20 @@ describe( 'NativeIface', function()
             ccm = new invoker.SimpleCCM( opts );
         });
         
+        afterEach(function( done ){
+            if ( httpsrv )
+            {
+                httpsrv.close( function(){ done(); } );
+                httpsrv = null;
+            }
+            else
+            {
+                done();
+            }
+        });
+        
         it( 'should return ifaceInfo without details', function(){
-            ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:12345' );
+            ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:23456' );
             var info = ccm.iface( 'myiface' ).ifaceInfo();
             ccm.iface( 'myiface' ).ifaceInfo().should.equal( info );
             
@@ -328,6 +561,37 @@ describe( 'NativeIface', function()
             info.inherits().length.should.equal( 0 );
             _.isEmpty( info.funcs() ).should.be.true;
             _.isEmpty( info.constraints() ).should.be.true;
+            
+            var iface = ccm.iface( 'myiface' );
+            
+            iface.should.not.have.property( 'testFunc' );
+            iface.should.not.have.property( 'rawUploadFunc' );
+        });
+        
+        it( 'should call HTTP remotes', function( done ){
+            as.add(
+                function(as){
+                    try {
+                        ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:23456/ftn' );
+                        
+                        as.add(function( as ){
+                            createTestHttpServer( function(){ as.success(); } );
+                            as.setTimeout( 100 );
+                        });
+                    } catch ( e ){
+                        console.dir( e.stack );
+                        console.log( as.state.error_info );
+                        throw e;
+                    }
+                },
+                function( as, err )
+                {
+                    as.state.done( new Error( err + ": " + as.state.error_info ) );
+                }
+            );
+            as.copyFrom( call_remotes_model_as );
+            as.state.done = done;
+            as.execute();
         });
     });
     
@@ -343,7 +607,7 @@ describe( 'NativeIface', function()
             as.add(
                 function(as){
                     try {
-                        ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:12345' );
+                        ccm.register( as , 'myiface', 'fileface.a:1.1', 'http://localhost:23456' );
                         as.successStep();
                     } catch ( e ){
                         console.dir( e.stack );
@@ -366,6 +630,19 @@ describe( 'NativeIface', function()
                         info.inherits().length.should.equal( 0 );
                         _.isEmpty( info.funcs() ).should.be.false;
                         _.isEmpty( info.constraints() ).should.be.false;
+                        
+                        var iface = ccm.iface( 'myiface' );
+                        
+                        iface.should.have.property( 'testFunc' );
+                        iface.should.not.have.property( 'rawUploadFunc' );
+                        
+                        assert.throws( function(){
+                            iface.burst();
+                        }, 'InvokerError' );
+                        
+                        assert.throws( function(){
+                            iface.bindDerivedKey();
+                        }, 'InvokerError' );
                         
                         done();
                         as.success();
